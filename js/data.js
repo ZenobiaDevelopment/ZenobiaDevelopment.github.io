41261c6c2a1fd039c3cf4bfcad3823d16f79cd19
grady2002 <aatifmastan644@gamail.com>
2022-07-15 17:17:56 +0530

    stage 1 complete, optimised for mobile devices

--- js/data.js
+++ js/data.js
@@ -1,11 +1,13 @@
 const data = [
   {
+    id: 1,
     name: "Helerius",
     url: "https://www.npmjs.com/package/helerius",
     about:
       "Powerfull NPM package that helps you to interact with the Viber API.",
   },
   {
+    id: 2,
     name: "Testament CLI",
     url: "https://www.npmjs.com/package/testament-cli",
     about: "Simple Javascript CLI tool to bring fun to your node console.",
